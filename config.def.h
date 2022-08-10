7d3a123102fdfbd81c947b42d2c60f35dcf8fd70
karsten gebbert <k@ioctl.it>
2022-08-10 17:14:28 +0200

    adding rules and setting monocle as default

--- config.def.h
+++ config.def.h
@@ -40,21 +40,27 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/* class      									instance    							title       tags mask     isfloating   monitor */
+	{ "Gimp",     									NULL,       							NULL,       0,            1,           -1 },
+	{ "org.wezfurlong.wezterm",     "org.wezfurlong.wezterm", NULL,       1 << 2,				0,           -1 },
+	{ "TelegramDesktop",				    "Telegram",       				NULL,       1 << 1,       0,           -1 },
+	{ "Signal",									    "signal",       					NULL,       1 << 1,       0,           -1 },
+	{ "Slack",									    "slack",       						NULL,       1 << 1,       0,           -1 },
+	{ "Google-chrome", 							"google-chrome",       		NULL,       1,            0,           -1 },
+	{ "Navigator", 									"firefox",       					NULL,       1,            0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "[M]",      monocle },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
 	{ "|||",      col },
 };
 
@@ -124,9 +130,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return,                 zoom,           {0} },
 	{ MODKEY,                       XK_Tab,                    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,                      killclient,     {0} },
-	{ MODKEY,                       XK_t,                      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,                      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,                      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,                      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_t,                      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,                      setlayout,      {.v = &layouts[2]} },
  	{ MODKEY,                       XK_c,                      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_f,                      fullscreen,     {0} },
 	{ MODKEY,                       XK_space,                  setlayout,      {0} },
